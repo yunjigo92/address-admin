83e15f4e85a8d83b29eff8cc7b2522c8afcd6e8a
yunji <yunjigo92@gmail.com>
2020-03-18 14:32:44 +0900

    spring boot & jpa 공부

---
+++ src/test/java/com/admin/adress/web/HelloControllerTest.java
@@ -0,0 +1,59 @@
+package com.admin.adress.web;
+
+import com.admin.adress.web.dto.HelloResponseDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void helloTest() throws Exception{
+        //hello가 리턴된다.
+
+        //given
+        String hello = "hello";
+
+        //when
+        ResultActions perform = mvc.perform(get("/hello"));
+
+        //then
+        perform
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+
+
+    @Test
+    public void helloDtoTest() throws Exception {
+        //helloDto가 return 된다.
+
+        //given
+        String name = "윤지";
+        int amount =1000;
+
+        //when
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+}
---
+++ src/test/java/com/admin/adress/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,27 @@
+package com.admin.adress.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest(){
+        //given
+        String name = "윤지";
+        int amount =1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+
+
+}
